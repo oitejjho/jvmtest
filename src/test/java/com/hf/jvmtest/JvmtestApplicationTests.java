93811d8a9fea0973bcb5d6069a9de886d8f496c9
oitejjhodelivery <oitejjhodutta@deliveryhero.com>
2024-02-16 16:17:22 +0100

    main branch push

---
+++ src/test/java/com/hf/jvmtest/JvmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hf.jvmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
